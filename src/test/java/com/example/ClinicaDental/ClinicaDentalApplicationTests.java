65c744961c2a88794681606ef92f2565a27b965f
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2022-07-11 00:09:58 -0300

    mejora en los test unitarios

--- src/test/java/com/example/ClinicaDental/ClinicaDentalApplicationTests.java
+++ src/test/java/com/example/ClinicaDental/ClinicaDentalApplicationTests.java
@@ -4,20 +4,22 @@ import com.example.ClinicaDental.entity.Domicilio;
 import com.example.ClinicaDental.entity.Odontologo;
 import com.example.ClinicaDental.entity.Paciente;
 import com.example.ClinicaDental.entity.Turno;
-import com.example.ClinicaDental.exceptions.ResourceNotFoundException;
 import com.example.ClinicaDental.service.OdontologoService;
 import com.example.ClinicaDental.service.PacienteService;
 import com.example.ClinicaDental.service.TurnoService;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ClinicaDentalApplicationTests {
 
     @Autowired
@@ -29,48 +31,41 @@ class ClinicaDentalApplicationTests {
     @Autowired
     TurnoService turnoService;
 
+    Domicilio domicilio = new Domicilio("Arena", 1010, "Buenos Aires", "Argentina");
+    Paciente paciente = new Paciente("Messi", "Lionel", "lionel@gmail.com", 1010, LocalDate.of(2020, 10, 10), domicilio);
+    Odontologo odontologo = new Odontologo("Gallardo", "Marcelo", 1);
+    Turno turno = new Turno(odontologo, paciente, LocalDateTime.of(2024,01,01,10,10));
+
     @Transactional
 	@Test
-	void testCrearPaciente() {
-        Domicilio domicilio = new Domicilio("Arena", 1010, "Buenos Aires", "Argentina");
-        Paciente paciente = new Paciente("Messi", "Lionel", "lionel@gmail.com", 1010, LocalDate.of(2020, 10, 10), domicilio);
-
+    @Order(1)
+	void testGuardarPaciente() {
         assertNotNull(pacienteService.guardar(paciente));
-
         assertNotNull(pacienteService.buscarPorEmail("lionel@gmail.com"));
 	}
 
     @Transactional
     @Test
-    void testBorrarPaciente() throws ResourceNotFoundException {
-        Paciente paciente = pacienteService.buscarPorEmail("lionel@gmail.com");
-
-        assertNull(pacienteService.eliminar(paciente.getId()));
-    }
-
-    @Transactional
-    @Test
-    void testListarOdontologo() {
-        Odontologo odontologo = new Odontologo("Gallardo", "Marcelo", 1);
-
+    @Order(2)
+    void testGuardarOdontologo() {
         assertNotNull(odontologoService.guardar(odontologo));
-
-        assertNotNull(odontologoService.listar());
+        assertNotNull(odontologoService.buscar(odontologo.getId()));
     }
 
     @Transactional
     @Test
+    @Order(3)
     void testCrearYBuscarTurno() {
         Domicilio domicilio = new Domicilio("Arena", 1010, "Buenos Aires", "Argentina");
         Paciente paciente = new Paciente("Messi", "Lionel", "lionel@gmail.com", 1010, LocalDate.of(2020, 10, 10), domicilio);
-        Odontologo odontologo = new Odontologo("Maradona", "Diego", 6666);
-
-        Turno turno = new Turno(odontologo, paciente, LocalDateTime.of(2022,01,01,10,10));
-
+        Odontologo odontologo = new Odontologo("Gallardo", "Marcelo", 1);
+        Turno turno = new Turno(odontologo, paciente, LocalDateTime.of(2024,01,01,10,10));
+        pacienteService.guardar(paciente);
+        odontologoService.guardar(odontologo);
         assertNotNull(turnoService.guardar(turno));
-
         assertNotNull(turnoService.buscar(turno.getId()));
     }
+
 
 
 }
